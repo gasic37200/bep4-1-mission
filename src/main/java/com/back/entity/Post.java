21d43d07092e395b6b2abdedcbdbba6fb15608a8
KimYoonGwan <dbsrhks2003@gmail.com>
2026-09-22 10:47:02 +0900

    0003: 글 댓글 작성 기능 및 초기 댓글 8개 생성

--- src/main/java/com/back/entity/Post.java
+++ src/main/java/com/back/entity/Post.java
@@ -4,8 +4,16 @@ import com.back.jap.entity.BaseIdAndTime;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.NoArgsConstructor;
 
+// 저장될 때 자식 엔티티도 같이 변경
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.CascadeType.PERSIST;
+// 삭제될 때 자식 엔티티도 같이 변경
+import static jakarta.persistence.CascadeType.REMOVE;
 import static jakarta.persistence.FetchType.LAZY;
 
 @Entity
@@ -17,9 +25,27 @@ public class Post extends BaseIdAndTime {
     @Column(columnDefinition = "LONGTEXT")
     private String content;
 
+    // orphanRemoval = 고아 객체(Orphan)를 자동으로 제거
+    // CascadeType.REMOVE와 달리 부모 엔티티가 삭제되거나 연관 관계가 삭제되면 같이 삭제됨
+    // = 가비지 컬렉터와 같은 개념
+    @OneToMany(mappedBy = "post", cascade = { PERSIST, REMOVE }, orphanRemoval = true)
+    private List<PostComment> comments = new ArrayList<>();
+
     public Post(Member author, String title, String content) {
         this.author = author;
         this.title = title;
         this.content = content;
+    }
+
+    public PostComment addComment(Member author, String content) {
+        PostComment postComment = new PostComment(this, author, content);
+
+        comments.add(postComment);
+
+        return postComment;
+    }
+
+    public boolean hasComments() {
+        return !comments.isEmpty();
     }
 }
